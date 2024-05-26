5549ee9377b26d2e58f760b388dd5c0e014b8e6a
Vinoth R <158076208+Vinothofficial21@users.noreply.github.com>
2024-05-26 20:54:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// scripts.js
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll('footer ul li a');
+
+    for (const link of links) {
+        link.addEventListener('click', smoothScroll);
+    }
+
+    function smoothScroll(event) {
+        event.preventDefault();
+        const targetId = event.currentTarget.getAttribute("href");
+        document.querySelector(targetId).scrollIntoView({
+            behavior: "smooth"
+        });
+    }
+
+    const sections = document.querySelectorAll('section');
+    const options = {
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
